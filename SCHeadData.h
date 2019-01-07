4a7a6a9a5d0dc33fc557bb937348acbb58ee7ed9
zjtdd <zhaojt163@163.com>
2019-01-07 19:28:33 +0800

    1.给SCHeadData.h添加报文的说明和相关注释。 2.解决用MinGw编译时操作数据库报错的问题。

--- SCHeadData.h
+++ SCHeadData.h
@@ -1,3 +1,13 @@
+﻿/** 报文格式示意图
+ *  |                                                 报文头部（16byte）                                                    |                  数据区域  (变长）      |
+ *  |  报文同步头  |  协议版本  |  序号  |  数据区长度  |  报文类型(编号)  |  保留区域  |                       数据区域                 |
+ *  |       1byte       |    1byte      | 2byte|       4byte       |           2byte          |     6byte     |  取决于头部中的数据区域长度  |
+ *  |        0x5a       |      0x01     | 0x00 |        0x00        |          0x00            |     0x00      |         JSON序列化数据内容        |
+ *
+ *  详细内容可查阅手册：https://docs.seer-robotics.com/robokit_netprotocol/663328
+ *
+  */
+
 #ifndef _SEER_PROTOCOL_V1_H_
 #define _SEER_PROTOCOL_V1_H_
 
@@ -7,12 +17,12 @@
 class SeerHeader{
 public:
     SeerHeader(){
-        m_header = 0x5A;
-        m_version = 0x01;
-        m_number = 0;
-        m_length = 0;
-        m_type = 0;
-        memset(m_reserved,0, 6);
+        m_header = 0x5A; // 1byte    报文同步头，用于确定报文头部的开始
+        m_version = 0x01; // 1byte    协议的主版本号，对于v1.x.x版本的协议均填0X01
+        m_number = 0; // 2byte    请求及响应的序号（0~65535），请求包与响应包的这个字段是相同的，API使用者自行填入序号。机器人对每个请求的响应都是使用这个序号
+        m_length = 0; // 4byte    数据区长度，即JSON序列化数据的长度
+        m_type = 0; // 2byte    表示报文的类型，即API的编号。
+        memset(m_reserved,0, 6); // 6byte    保留区域。不必关注保留区的内容，其内容可能不为0且可能会发生变化
     }
 
     uint8_t m_header;
@@ -37,7 +47,7 @@ public:
         m_header.m_version = 0x01;
         qToBigEndian(type, (uint8_t*)&(m_header.m_type));
         qToBigEndian(number, (uint8_t*)&(m_header.m_number));
-        memset(m_header.m_reserved,0, 6);
+        memset(m_header.m_reserved, 0, 6);
         if (data != NULL) {
             memcpy(m_data, data, size);
         }
